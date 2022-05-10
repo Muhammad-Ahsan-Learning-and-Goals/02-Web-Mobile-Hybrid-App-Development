2e0a45eb74e0c06e7c12b214e5dc3a442bee4f7d
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-05-10 14:49:10 +0500

    Update App.js

--- 04-Code with Harry React Js/my-app/src/App.js
+++ 04-Code with Harry React Js/my-app/src/App.js
@@ -9,6 +9,14 @@ function App() {
         <li>About</li>
         <li>Contect</li>
       </nav>
+      <div className="container">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, vitae
+          voluptatum? Voluptas unde laudantium repellat cupiditate dicta?
+          Tempore sapiente nobis quia ea. Dolor blanditiis esse magnam. Quo
+          eaque sequi voluptates!
+        </p>
+      </div>
     </>
   );
 }
