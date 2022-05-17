24c4eef5956f8ffd20d873aba2b6824e75eb5dd7
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-05-17 12:10:25 +0500

    Update App.js

--- 05- MERN Stack Web Series Developer Student Clubs - SSUET/myapp/src/App.js
+++ 05- MERN Stack Web Series Developer Student Clubs - SSUET/myapp/src/App.js
@@ -4,17 +4,20 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 class App extends React.Component {
-   state={
-   setCount : 0,
-  }
+  state = {
+    setCount: 10,
+  };
 
-} render(){
-  return ( 
-    <div className="App">
-      <h1>Counter App</h1>
-      <h2>{state.setCount}</h2>
-      <button>+</button>
-      <button>-</button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <h2>{this.state.setCount}</h2>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    );
   }
+}
+
+export default App;
