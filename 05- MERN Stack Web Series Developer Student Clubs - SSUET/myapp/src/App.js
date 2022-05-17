8e54f8bf0b3ed64a2956611f8b70962b24bb8963
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-05-17 11:28:50 +0500

    Update App.js

--- 05- MERN Stack Web Series Developer Student Clubs - SSUET/myapp/src/App.js
+++ 05- MERN Stack Web Series Developer Student Clubs - SSUET/myapp/src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      
+
+
+      
+    </div>
+  );
 }
 
 export default App;
