2ed2d26c1c9a2eeec703169357a4f1e5450836a3
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-05-27 00:59:33 +0500

    Update App.js

--- 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/App.js
+++ 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/App.js
@@ -4,9 +4,13 @@ import "./App.css";
 function App() {
   let fname = "Muhammad";
   let lname = "Ahsan";
+
+  let arr = [1, 2, 3, 4, 5];
   return (
     <div>
       <h1>{`${fname} ${lname}`}</h1>
+
+      <h1> {arr}</h1>
     </div>
 
     /* <h1> Hello World</h1> */
