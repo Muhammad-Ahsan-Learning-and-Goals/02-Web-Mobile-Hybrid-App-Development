f3274e540b96bf9c4df8b63bc7371c958a298e21
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-06-08 17:09:51 +0500

    Update App.js

--- 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/App.js
+++ 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/App.js
@@ -1,20 +1,67 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  let fname = "Muhammad";
-  let lname = "Ahsan";
+ let todo= [
+      { fname:"Ali"},
+      {lname:"Raza"}
+      {age:"54"},
 
-  let arr = [1, 2, 3, 4, 5];
-  return (
+  ];
+  return(
     <div>
-      <h1>{`${fname} ${lname}`}</h1>
+      <ul>
+        {todo.map((item, index)=>{
+          return<li>{item.text}</li>
 
-      <h1> {arr}</h1>
+        })}
+      </ul>
     </div>
 
-    /* <h1> Hello World</h1> */
-  );
-}
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   let fname = "Muhammad";
+//   let lname = "Ahsan";
+
+//   let arr = [1, 2, 3, 4, 5];
+//   return (
+//     <div>
+//       <h1>{`${fname} ${lname}`}</h1>
+
+//       <h1> {arr}</h1>
+//     </div>
+
+//     /* <h1> Hello World</h1> */
+   
+//   );
+// }
 
 export default App;
