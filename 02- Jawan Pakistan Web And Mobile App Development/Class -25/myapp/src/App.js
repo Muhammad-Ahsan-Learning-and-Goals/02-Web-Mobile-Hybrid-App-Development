97dd84d546f65e4b1e352bdcc70433cb1d882537
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-06-09 09:38:38 +0500

    update class

--- 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/App.js
+++ 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/App.js
@@ -9,27 +9,24 @@ class App extends Component {
   render() {
     return (
       <div>
+        <BasicTextFields />
         <Header />
         <h1 style={{ color: "red" }}>Main Components</h1>
         <img
           src="https://static.remove.bg/remove-bg-web/669d7b10b2296142983fac5a5243789bd1838d00/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png"
           alt=""
         />
-
         <Footer />
-
-        <BasicTextFields />
-
         <button type="button" className="btn btn-primary ">
           Primary
         </button>
-
+        <BasicTextFields />
         <h1> </h1>
       </div>
     );
   }
 }
-import BasicTextFields from "./componets/TextField";
+// import BasicTextFields from "./componets/TextField";
 
 // class Header extends Component {
 //   render() {
