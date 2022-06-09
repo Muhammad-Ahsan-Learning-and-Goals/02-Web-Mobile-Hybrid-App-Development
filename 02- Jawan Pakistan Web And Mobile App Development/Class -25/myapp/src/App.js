61eedafa38fd2b576fa96e2a7234360bd824767e
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-06-09 08:30:51 +0500

    update

--- 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/App.js
+++ 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/App.js
@@ -6,9 +6,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header /> */}
         <h1 style={{ color: "red" }}>Main Components</h1>
-        <Footer />
+        <img
+          src="https://static.remove.bg/remove-bg-web/669d7b10b2296142983fac5a5243789bd1838d00/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png"
+          alt=""
+        />
+        {/* <Footer /> */}
 
         <h1> </h1>
       </div>
@@ -16,25 +20,25 @@ class App extends Component {
   }
 }
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="Header">
-        <h1>Header</h1>
-      </div>
-    );
-  }
-}
+// class Header extends Component {
+//   render() {
+//     return (
+//       <div className="Header">
+//         <h1>Header</h1>
+//       </div>
+//     );
+//   }
+// }
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="Footer">
-        <h1>Footer</h1>
-      </div>
-    );
-  }
-}
+// class Footer extends Component {
+//   render() {
+//     return (
+//       <div className="Footer">
+//         <h1>Footer</h1>
+//       </div>
+//     );
+//   }
+// }
 
 // class App extends Component {
 //   render() {
---
+++ 02- Jawan Pakistan Web And Mobile App Development/Class -25/myapp/src/componets/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <View>
+        <div className="header">
+          <Text>Header</Text>
+        </div>
+      </View>
+    );
+  }
+}
+
+export default Header;
