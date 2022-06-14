da6568c958fa1efe88eaff62986bc62a2f604e37
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-06-14 11:31:17 +0500

    Update App.js

--- 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/App.js
+++ 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>App Component</h1>
-      </div>
-    );
-  }
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1> Class App Component</h1>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  return (
+    <div>
+      <h1> Function App Component </h1>
+    </div>
+  );
 }
 
 export default App;
