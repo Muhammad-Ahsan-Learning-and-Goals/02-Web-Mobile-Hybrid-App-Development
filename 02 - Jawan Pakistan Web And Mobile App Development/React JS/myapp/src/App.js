492dcdf4841b9e7bb9a5813b7ffb9f22e603c738
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-06-14 09:46:35 +0500

    update file

--- 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/App.js
+++ 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/App.js
@@ -1,18 +1,18 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+import { Header, Logo } from "./components/Header";
 import { render } from "@testing-library/react";
-
-
+ 
 class App extends Component {
   render() {
-    return(
-
-     <div>
-     <Header/>
-    </div>
-    )
+    return (
+      <div>
+        <Header />
+        <Logo />
+      </div>
+    );
   }
 }
 
--- 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/components/Header.js
+++ 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/components/Header.js
@@ -9,4 +9,16 @@ class Header extends React.Component {
     );
   }
 }
-export default Header;
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Logo </h1>
+        <img src="" alt="" />
+      </div>
+    );
+  }
+}
+
+export { Header, Logo };
