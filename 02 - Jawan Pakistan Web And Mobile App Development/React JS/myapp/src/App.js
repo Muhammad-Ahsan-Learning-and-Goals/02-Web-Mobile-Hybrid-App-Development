85a16e824439d909602e1abc7c8e2b2df2d990ec
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-06-14 08:27:25 +0500

    update files

--- 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/App.js
+++ 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { render } from '@testing-library/react';
+
 
 function App() {
+  render(){
   return (
-    
+    <div>
+      <h2> App Components</h2>
+    </div>
+     
      
   )
 }
-
+}
 export default App;
