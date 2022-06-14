61ed3d8e66bb1f6ad0c1216981f211c2ecc62c33
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-06-14 11:21:52 +0500

    update code

--- 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/App.js
+++ 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/App.js
@@ -1,16 +1,10 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Footer from "./components/Footer";
-import { Header, Logo } from "./components/Header";
-import { render } from "@testing-library/react";
- 
-class App extends Component {
+import React from "react";
+
+class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <Logo />
+        <h1>App Component</h1>
       </div>
     );
   }
