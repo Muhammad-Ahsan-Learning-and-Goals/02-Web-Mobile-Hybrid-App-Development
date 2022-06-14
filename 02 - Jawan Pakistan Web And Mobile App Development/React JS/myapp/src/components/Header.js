082f179c5586d53d503ef9ea039325d25f4b2b5e
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-06-14 10:45:26 +0500

    updte

--- 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/components/Header.js
+++ 02 - Jawan Pakistan Web And Mobile App Development/React JS/myapp/src/components/Header.js
@@ -1,24 +1,27 @@
 import React from "react";
 
+import Logo from "../images/is.jpg";
+
 class Header extends React.Component {
   render() {
     return (
       <div className="header">
         <h1>Header</h1>
+        <Logo />
       </div>
     );
   }
 }
 
-class Logo extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1> Logo </h1>
-        <img src="" alt="" />
-      </div>
-    );
-  }
-}
+// class Logo extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1> Logo </h1>
+//         <img src={Logo} alt="car"  width="50" />
+//       </div>
+//     );
+//   }
+// }
 
 export { Header, Logo };
