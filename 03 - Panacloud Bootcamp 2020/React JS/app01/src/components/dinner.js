cc0c9994d4df46a90dce698ef2b709246916fb11
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-09-13 10:46:00 +0500

    update

---
+++ 03 - Panacloud Bootcamp 2020/React JS/app01/src/components/dinner.js
@@ -0,0 +1,13 @@
+    import React from "react";
+    function Dinner() {
+    return (
+    <div>
+    <h3>Today We are Serving Chicken biryani</h3>
+
+    <h3>Today We are Serving Biryani</h3>
+    </div>
+
+    ); 
+    }
+
+export default Dinner;
