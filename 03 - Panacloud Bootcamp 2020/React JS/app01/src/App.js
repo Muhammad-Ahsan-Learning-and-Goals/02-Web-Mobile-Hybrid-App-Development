a4fa9e523e07cb706e33a3500808b7c88c2fdef3
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-09-13 13:25:50 +0500

    update

--- 03 - Panacloud Bootcamp 2020/React JS/app01/src/App.js
+++ 03 - Panacloud Bootcamp 2020/React JS/app01/src/App.js
@@ -5,9 +5,9 @@ import Dinner from "./components/dinner.js";
 function App() {
   return (
     <div className="App">
-        <h2> This is main Page</h2>
-        
-      <Dinner />
+      <h2> This is main Page</h2>
+
+      <Dinner dishName=" Sabzi" sweet=" Kheer" />
     </div>
   );
 }
--- 03 - Panacloud Bootcamp 2020/React JS/app01/src/components/dinner.js
+++ 03 - Panacloud Bootcamp 2020/React JS/app01/src/components/dinner.js
@@ -1,13 +1,16 @@
-    import React from "react";
-    function Dinner() {
-    return (
+import React from "react";
+
+function Dinner(props){
+  return (
     <div>
-    <h3>Today We are Serving Chicken biryani</h3>
+    
+      <h3>Today We are Serving{props.dishName}</h3>
 
-    <h3>Today We are Serving Biryani</h3>
+      <hr />
+
+      <h3>Today We are Serving{props.sweet}</h3>
     </div>
-
-    ); 
-    }
+  );
+}
 
 export default Dinner;
