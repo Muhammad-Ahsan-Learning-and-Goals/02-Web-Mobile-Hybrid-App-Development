876fea6afe2871265f1cfaea54a153c9827ef052
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-11-29 18:40:35 +0500

    update class

--- 01 - Bootcamp - 2020/02 - React Part Practice/firstapp/src/App.js
+++ 01 - Bootcamp - 2020/02 - React Part Practice/firstapp/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Dinner from "./components/dinner.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dinner dishName="biryani" sweetDish="Kheer" />
+      <Dinner dishName="Chicken" sweetDish="Halwa" />
+      <Dinner dishName="Mutton" sweetDish="Mango" />
     </div>
   );
 }
---
+++ 01 - Bootcamp - 2020/02 - React Part Practice/firstapp/src/components/dinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dinner(props) {
+  return(
+  <h3>
+    Today We are Going to Eat {props.dishName} and {props.sweetDish}
+    </h3>
+  
+  )
+}
+
+export default Dinner;
