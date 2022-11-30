33911964a23c27a698cf86fb4d263ee7937ebf78
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-11-30 19:25:26 +0500

    update counter App

---
+++ 01 - Bootcamp - 2020/02 - React Part Practice/firstapp/src/components/counter.js
@@ -0,0 +1,28 @@
+// import React, { useState, isMorning, Morning, Night } from "react";
+
+// function Message() {
+//   let [count, setCount] = useState(1);
+//   return (
+//     <>
+//       <h4> Value of Count is : {count}</h4>
+
+//       <button onClick={() => setCount(count + 1)}>Update Counter</button>
+//     </>
+//   );
+// }
+// export default Message;
+
+//Pracice 02
+
+// import React, { useState } from "react";
+// function Counter() {
+//   let [count, setCount] = useState(1);
+//   return (
+//     <>
+//       <h1> The value is {count}:</h1>
+//       <button onClick={() => setCount(count + 2)}> Update Counter</button>
+//     </>
+//   );
+// }
+
+// export default Counter;
--- 01 - Bootcamp - 2020/02 - React Part Practice/firstapp/src/components/dinner.js
+++ 01 - Bootcamp - 2020/02 - React Part Practice/firstapp/src/components/dinner.js
@@ -1,24 +1,12 @@
-// import React from "react";
-
-// function Dinner(props) {
-//   return (
-//     <>
-//       <h3>i like to eat {props.sweetDish}</h3>
-
-//       <h3>i like to eat {props.dishName}</h3>
-//     </>
-//   );
-// }
-
-// export default Dinner;
-
 import React from "react";
 function Dinner(props) {
   return (
-    <>
-      <h3> I likes to Eat {props.sweetDish}</h3>
-      <h4> I Do not Like to eat {props.dishName}</h4>
+      <>
+      <h1>I Eat {props.sweetDish} and {props.dishName} in brackFast </h1>
+      <h1>  I Eat {props.sweetDish} and {props.dishName} in  Lunch</h1>
+      <h1>  I Eat {props.sweetDish} and {props.dishName} in Dinner</h1>
     </>
   );
 }
+
 export default Dinner;
