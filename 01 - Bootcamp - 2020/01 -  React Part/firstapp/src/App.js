4c97acadd60cb1f91bf027ab2cf6f87985165bd1
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-11-30 17:40:59 +0500

    update

--- 01 - Bootcamp - 2020/01 -  React Part/firstapp/src/App.js
+++ 01 - Bootcamp - 2020/01 -  React Part/firstapp/src/App.js
@@ -1,23 +1,36 @@
+import React, { useState, isMorning, Morning, Night } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 import Dinner from "./components/dinner.js";
 import App1 from "./components/class3part2";
+import Message from "./components/messagePass";
 
 function App() {
+  let [count, setCount] = useState(1);
+  
+  let [isMorning, setMorning] = useState(true);
+
   return (
-    <div className="App">
-      {/* <Dinner dishName="Mutton" sweetDish="Halwa" />
+    //  <div className="App">
+    //   <Dinner dishName="Mutton" sweetDish="Halwa" />
 
-      <hr />
+    //   <hr />
 
-      <Dinner dishName="Pallao" sweetDish="Kheer" />
+    //   <Dinner dishName="Pallao" sweetDish="Kheer" />
 
-      <hr />
+    //   <hr />
 
-      <Dinner dishName="gost" sweetDish="sweet" /> */}
+    //   <Dinner dishName="gost" sweetDish="sweet" />
 
-      <App1 />
-      
+    <div className={`box ${isMorning ? `dayLight` : " "}`}>
+      <h2>Have A Good  {isMorning ? "Morning" : "Night"}</h2>
+
+      <Message counter={count} />
+      <br />
+      <button onClick={() => setCount(count + 1)}>Update Counter</button>
+
+      <button onClick={() => setMorning(!isMorning)}>Have a Good Morning</button>
     </div>
   );
 }
