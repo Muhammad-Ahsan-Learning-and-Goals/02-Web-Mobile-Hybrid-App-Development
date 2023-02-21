eb9576068c79e266475a2d4a5a940da5e39f5736
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-02-21 15:45:44 +0500

    update class

--- 04 -  JavaScript/basic concepts.js
+++ 04 -  JavaScript/basic concepts.js
@@ -45,3 +45,39 @@ document.write(d);
 // let z = prompt("What is Your Name");
 
 // document.write(z);
+
+// Objects in Javascript
+
+let person = {
+  firstName: "Ali",
+  lastName: "Raza",
+  age: 20,
+};
+
+console.log(person.firstName);
+
+const persons = [
+  {
+    firstName: "Hamzah",
+    lastName: "Syed",
+    age: 22,
+    "eye-Color": "brown",
+  },
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    "eye-Color": "blue",
+  },
+];
+// Access Person 1
+
+console.log(persons[0].age); // 22
+console.log(persons[1].age); // 50
+
+for (let i = 0; i < 3; i++) {
+  document.write(" Hello World <br>" + i);
+}
+
+const date = new Date();
+console.log(date);
