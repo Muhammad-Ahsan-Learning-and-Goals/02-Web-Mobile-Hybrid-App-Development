50fd3100ae86950e9888dd0f5b5535f4677637eb
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-02-21 13:37:09 +0500

    update

---
+++ 04 -  JavaScript/app.js
@@ -0,0 +1,47 @@
+// alert("This is pop Up Alert Box");
+
+// console.log("This is console log ");
+
+// document.write("Text will Print on Screen");
+
+// variables
+
+// var let and const    Can  be Re Declear
+
+var a = "Ali ";
+document.write(a);
+
+var a = "Raza";
+
+document.write(a);
+
+// let varivable type    // Can not Re Declear
+
+document.write("<br>");
+
+// let b = "Muhammad ";
+
+// document.write(b);
+
+// let b = "Ahsan";
+// document.write(b);
+
+document.write("<br>");
+
+// const varivable type    // Can not be Re Declear or re assign
+
+const c = "Java  ";
+
+document.write(c);
+
+const d = "Javascript";
+
+document.write(d);
+
+// document.write(typeof d);
+
+// prompt
+
+// let z = prompt("What is Your Name");
+
+// document.write(z);
