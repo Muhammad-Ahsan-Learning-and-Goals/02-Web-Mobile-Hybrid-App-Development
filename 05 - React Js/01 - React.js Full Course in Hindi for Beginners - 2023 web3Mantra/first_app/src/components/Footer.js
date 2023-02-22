bbd7deda6ebabeb50f22876809d6c6ff07dbca42
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-02-22 14:48:25 +0500

    update class

---
+++ 05 - React Js/01 - React.js Full Course in Hindi for Beginners - 2023 web3Mantra/first_app/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <h2>Footer component</h2>
+      </div>
+    );
+  }
+}
+export default Footer;
+
+// hm class k sath bi export default add kr skty han
