bcb59585cf0eae4daa82d419bf6722ddb21d93d3
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-02-23 15:09:32 +0500

    v

--- 05 - React Js/02 - 🎯 React Tutorial in Hindi for Beginners  Complete React JS Tutorial with Project  2023 Best Course/first_app/src/App.js
+++ 05 - React Js/02 - 🎯 React Tutorial in Hindi for Beginners  Complete React JS Tutorial with Project  2023 Best Course/first_app/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 
+import SubComponent1 from "./components/SubComponent1";
+
 function App() {
   return (
     <div className="cotainer1">
       <h3 className="cotainer2"> First component </h3>
       <p className="cotainer3"> This is Pargraph Tag</p>
+
+      <SubComponent1 />
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ 05 - React Js/02 - 🎯 React Tutorial in Hindi for Beginners  Complete React JS Tutorial with Project  2023 Best Course/first_app/src/components/SubComponent1.js
@@ -0,0 +1,10 @@
+import "./SubComponent.css";
+
+function SubComponent1() {
+  return (
+  <p className="red"> This is Sub Component</p>
+)
+}
+
+export default SubComponent1;
+ 
