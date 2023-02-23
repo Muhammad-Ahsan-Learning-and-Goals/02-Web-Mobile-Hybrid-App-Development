e9d625a3d4167d5b16fbd5a98762bcdc2e884906
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-02-23 14:42:53 +0500

    update code

---
+++ 05 - React Js/02 - 🎯 React Tutorial in Hindi for Beginners  Complete React JS Tutorial with Project  2023 Best Course/first_app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="cotainer1">
+      <h3 className="cotainer2"> First component </h3>
+      <p className="cotainer3"> This is Pargraph Tag</p>
+    </div>
+  );
+}
+
+export default App;
--- 05 - React Js/02 - 🎯 React Tutorial in Hindi for Beginners  Complete React JS Tutorial with Project  2023 Best Course/first_app/src/index.js
+++ 05 - React Js/02 - 🎯 React Tutorial in Hindi for Beginners  Complete React JS Tutorial with Project  2023 Best Course/first_app/src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 // 🌟 Attach Universal CSS in React 
 
 import "./index.css";
+import App from './App';
 
-ReactDOM.render(<h3>Hello React</h3>, document.getElementById("root"));
+// ReactDOM.render(<h3>Hello React</h3>, document.getElementById("root"));
+
+ReactDOM.render(<App/>, document.getElementById("root"));
