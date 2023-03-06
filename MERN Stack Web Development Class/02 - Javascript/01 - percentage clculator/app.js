f07c3d2b878779cae95fb5c886bcf0a7ed6a485c
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-03-06 16:32:23 +0500

    upadte

---
+++ MERN Stack Web Development Class/02 - Javascript/01 - percentage clculator/app.js
@@ -0,0 +1,18 @@
+// 🌟 find percentage of marks
+
+// let totalMarks = 1000;
+// let obtainMarks = 690;
+
+// let percentage = (obtainMarks / totalMarks) * 100;
+
+// console.log(percentage);
+
+// 🌟🌟  Marks percentage calculator
+
+// let totalMarks = prompt("Enter Total Marks");
+
+// let obtainedMarks = prompt("Enter Obtained Marks");
+
+// let percentage = (obtainedMarks / totalMarks) * 100;
+
+// window.alert(percentage + " % ");
