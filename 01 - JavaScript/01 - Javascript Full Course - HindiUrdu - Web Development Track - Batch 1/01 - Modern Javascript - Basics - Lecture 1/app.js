81588dfd41d67aee7c1874a8119c9cac5461b959
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-03-09 15:57:30 +0500

    update JS code

---
+++ 01 - JavaScript/01 - Javascript Full Course - HindiUrdu - Web Development Track - Batch 1/01 - Modern Javascript - Basics - Lecture 1/app.js
@@ -0,0 +1,14 @@
+//  permitive and Refrance Type Values
+
+let latname = "Ahsan"; // String Litrals 
+
+let age = 23; // Nuber Litrals
+
+let isApproved =true ; // boolean Litral
+
+let thisVAribale  = undefined ;
+
+let homeAddress= null ; //
+
+
+
