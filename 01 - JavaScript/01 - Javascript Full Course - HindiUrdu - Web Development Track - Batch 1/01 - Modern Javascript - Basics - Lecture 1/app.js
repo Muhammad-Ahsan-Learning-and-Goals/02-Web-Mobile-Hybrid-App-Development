b122ce7bc62df3b31dc3a66d86dd8b025d2b00e0
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-03-10 10:54:42 +0500

    a

--- 01 - JavaScript/01 - Javascript Full Course - HindiUrdu - Web Development Track - Batch 1/01 - Modern Javascript - Basics - Lecture 1/app.js
+++ 01 - JavaScript/01 - Javascript Full Course - HindiUrdu - Web Development Track - Batch 1/01 - Modern Javascript - Basics - Lecture 1/app.js
@@ -1,14 +1,56 @@
 //  permitive and Refrance Type Values
 
-let latname = "Ahsan"; // String Litrals 
+let latname = "Ahsan"; // String Litrals
 
 let age = 23; // Nuber Litrals
 
-let isApproved =true ; // boolean Litral
+let isApproved = true; // boolean Litral
 
-let thisVAribale  = undefined ;
+let thisVAribale = undefined;
 
-let homeAddress= null ; //
+let homeAddress = null; //
 
+let num1 = "Ahsan";
+
+// 🌟 method 1
+
+// console.log(typeof num1 === "number" ? true : false);
+
+// 🌟🌟 method 2
+
+// if (typeof num1 === "number") {
+//   console.log("Product is Number");
+// } else {
+//   alert("Please Add a Number");
+// }
+
+// permitive data type  int  double , float, number
+
+// refrance  data type  objects , numbers , arrays
+
+let person = {
+  fName: "Muhammad",
+  lName: "Ahsan",
+  age: 23,
+};
+
+// Dot Notation
+
+// console.log(person.fName);
+
+// Bracket Notation
+
+// console.log(person["lName"]);
+
+// 🌟🌟 Arrays
+
+let array1 = [1, 2, "Ali", true, null, "Raza"];
+
+console.log(array1);
+
+console.log(array1[3]);
+
+array1[7] = "Java";
+console.log(array1);
 
 
