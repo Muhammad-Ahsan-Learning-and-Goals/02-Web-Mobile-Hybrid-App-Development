463792df85cf910c88ce44085232024d139a3e96
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-03-10 17:42:24 +0500

    Update app2.js

--- 01 - JavaScript/01 - Javascript Full Course - HindiUrdu - Web Development Track - Batch 1/02 - Modern Javascript - Basics (2) - Lecture 2/app2.js
+++ 01 - JavaScript/01 - Javascript Full Course - HindiUrdu - Web Development Track - Batch 1/02 - Modern Javascript - Basics (2) - Lecture 2/app2.js
@@ -1,3 +1,19 @@
+// 🌟Array Method in js
 
-// Array Method in js
+let random = ["ali", "raza", "umer", 54, true];
+console.log(typeof random);
 
+// 🌟🌟 join Method
+
+let arrayjoin = random.join(" , ");
+
+console.log(arrayjoin);
+
+console.log(typeof arrayjoin);
+
+// 🌟🌟 Join Complete array
+
+
+let joinarr = random.join(" ");
+
+console.log(joinarr);
