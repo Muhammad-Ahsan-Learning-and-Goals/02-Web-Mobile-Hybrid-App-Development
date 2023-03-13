55bab581833f9baa9fe94c46519bc8daefebf583
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-03-13 17:24:38 +0500

    Update

---
+++ MERN Stack Web Development Class/02 - Javascript/00 -  Practice/04 -  Arrow function/app.js
@@ -0,0 +1,45 @@
+
+
+
+// hositing
+
+console.log(sumTwoNumbers(10, 5));
+
+function sumTwoNumbers(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+
+
+
+// aanniminus Function no aoating
+
+let myFunction = function (num1, num2) {
+  let result = num1 + num2;
+  return result;
+};
+
+console.log(myFunction(10, 20));
+
+
+
+
+// Arrow Function es6
+
+let myFunction1 = (num1, num2) => {
+  let result = num1 + num2;
+  return result;
+};
+
+console.log(myFunction1(40, 10));
+
+let myFunction2 = (num1, num2) => num1 + num2;
+
+console.log(myFunction2(5, 80));
+
+
+
+let findCube = (num) => num * num * num;
+ 
+console.log(findCube(6));
+ 
