67920a2f8f835a4c32fb7e40bbcd6ca3b9f53f27
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-03-20 13:23:37 +0500

    Upadate React App

--- React.js Full Course in Hindi for Beginners - 2023  Master React in 12 Hours web3Mantra/first_project/src/App.tsx
+++ React.js Full Course in Hindi for Beginners - 2023  Master React in 12 Hours web3Mantra/first_project/src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "./App.css";
+import { Header, Footer } from "./components/Header";
 
 function App() {
+  return (
+    <div>
+      <Header />
 
-  return <div>App</div>;
+      <h2> Reacts js </h2>
 
+      <Footer />
+
+    </div>
+  );
 }
 
 export default App;
---
+++ React.js Full Course in Hindi for Beginners - 2023  Master React in 12 Hours web3Mantra/first_project/src/components/Header.tsx
@@ -0,0 +1,20 @@
+// const Header = () => {
+
+function Header() {
+  return (
+    <>
+      <h3>This is Header component</h3>
+    </>
+  );
+}
+
+function Footer(){
+  return(
+
+    <h3> This is Footer Component</h3>
+
+
+  )
+
+}
+export { Header , Footer };
