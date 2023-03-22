d4f48a4f28b2451814b3c1991abdd11496a80720
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-03-22 10:13:21 +0500

    add hooks react counter app

--- React.js Full Course in Hindi for Beginners - 2023  Master React in 12 Hours web3Mantra/first_project/src/components/Hooks/Hook.tsx
+++ React.js Full Course in Hindi for Beginners - 2023  Master React in 12 Hours web3Mantra/first_project/src/components/Hooks/Hook.tsx
@@ -1,13 +1,23 @@
 import "./Hook.css";
+import React, { useState } from "react";
 
 function Hook() {
+
+  const
   return (
-    <div className="hook1">
+    <div>
+      <div className="hook1">
+        <h3>Hooks in React js</h3>
+      </div>
 
-      <h3>Hooks in React js</h3>
-
+      <div className="hook2">
+        <h2> USEState Hook</h2>
+        <h2 className="heading1">1</h2>
+        <button className="btn"> Increment</button>
+        <button className="btn">Decrement</button>
+      </div>
     </div>
   );
-};
+}
 
 export default Hook;
