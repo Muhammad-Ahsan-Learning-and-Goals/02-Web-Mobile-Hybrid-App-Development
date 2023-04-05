cadf81f70c46cbb42f6193f4916997fa2c2c1ee6
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-04-05 11:16:26 +0500

    h

--- Learning Never Ends/first_project/src/App.jsx
+++ Learning Never Ends/first_project/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { App1, Propss } from "./components/statements/Prptypes";
+import Arrowf from "./components/functions/Arrowf";
 import "./App.css";
 
 function App() {
@@ -9,16 +10,26 @@ function App() {
         <h2 className="App">Main App Component</h2>
       </div>
 
-      <App1 />
+      {/* <App1 /> */}
 
-      <div>
-        <Propss
+
+       {/* <Propss
           fname="Ahsann"
           age={23}
           class=" React Bootcamp"
           arr={[1, 2, 3]}
-        />
-      </div>
+        /> */}
+
+
+      <Arrowf />
+      
+
+       <div>
+
+
+
+      
+      </div> 
     </>
   );
 }
---
+++ Learning Never Ends/first_project/src/components/functions/Arrowf.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Arrowf = () => {
+  return (
+    <div>Arrowf</div>
+  )
+}
+
+export default Arrowf
