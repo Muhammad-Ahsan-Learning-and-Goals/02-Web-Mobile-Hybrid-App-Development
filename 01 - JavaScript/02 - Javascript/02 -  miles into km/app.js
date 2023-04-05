949987720637a5fdd50269f0a81d90583955127c
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-04-05 15:45:12 +0500

    12

--- 01 - JavaScript/02 - Javascript/02 -  miles into km/app.js
+++
@@ -1,5 +0,0 @@
-let miles = parseInt(prompt("Enter Your Value in Miles"));
-
-let km = miles * 1.6;
-
-document.write( miles + " is equal to " + km + " Kilometers ");
--- Learning Never Ends/first_project/src/components/functions/Arrowf.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Arrowf = () => {
-  return (
-    <div>Arrowf</div>
-  )
-}
-
-export default Arrowf
