5e87548b8a12b1dcc15a6625967beb9784218dd0
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-04-12 14:09:55 +0500

    update app

--- 03 - 6 Pack Programmer/01 - REACT JS COURSE 2023/01-project-one/components/header/Header.jsx
+++ 03 - 6 Pack Programmer/01 - REACT JS COURSE 2023/01-project-one/components/header/Header.jsx
@@ -1,30 +1,41 @@
 import React from "react";
+import "./Header.css"
 
 const Header = () => {
   // how to include css in file
   //  inline and Internal CSS
 
   return (
-    <div
-      style={{
-        backgroundColor: "red",
-        padding: "2px",
-        alignItems: "center",
-        textAlign: "center",
-        color: "white",
-        fontSize: "bold",
-        marginBottom: "10px",
-      }}
-    >
-      <h3
+    <>
+      <div className="header"
         style={{
-          border: "2px solid white",
-          padding: "10px",
+          backgroundColor: "red",
+          padding: "2px",
+          alignItems: "center",
+          textAlign: "center",
+          color: "white",
+          fontSize: "bold",
+          marginBottom: "10px",
         }}
       >
-        Header
-      </h3>
-    </div>
+        <h3
+          style={{
+            border: "2px solid white",
+            padding: "10px",
+          }}
+        >
+          Header
+        </h3>
+      </div>
+
+      <div className="header1">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+
+
+      </div>
+    </>
   );
 };
 
--- 03 - 6 Pack Programmer/01 - REACT JS COURSE 2023/01-project-one/src/App.jsx
+++ 03 - 6 Pack Programmer/01 - REACT JS COURSE 2023/01-project-one/src/App.jsx
@@ -3,16 +3,19 @@ import JSX from "../components/import and jsx/Import";
 import Header from "../components/header/Header";
 import Forms from "../components/Forms and UseState/Forms";
 import Router from "../components/router/Router";
+import Routes from "../components/routing concept/Routes";
 
 function App() {
   return (
     <div>
       {/* <JSX /> */}
-      {/* <Header /> */}
+      <Header />
 
       {/* <Forms /> */}
 
-      <Router/>
+      {/* <Router/> */}
+
+      <Routes />
     </div>
   );
 }
