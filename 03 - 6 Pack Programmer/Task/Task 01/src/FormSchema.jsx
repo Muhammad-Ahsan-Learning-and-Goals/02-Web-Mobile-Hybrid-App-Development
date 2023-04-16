96e6183dc0050cffa376eb5a9d348486400928e5
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-04-16 16:35:33 +0500

    update app

--- 03 - 6 Pack Programmer/Task/Task 01/src/FormSchema.jsx
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-
-
-export const FormScchema= Yup.object({
-    name:Yup.string().min(3).max(20).required('Name is Must'),
-    
-    email:Yup.string().email().required('Email is must')
-
-});
