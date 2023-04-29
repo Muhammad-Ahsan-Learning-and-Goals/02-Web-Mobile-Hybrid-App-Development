973683bc33e18bb64af13785cb4d57123517bd3c
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-04-29 19:28:27 +0500

    update app

--- 02 - 6 Pack programmer/01 - MASTER REACT JS IN ONE VIDEO WITH 5 PROJECTS/project-01/src/App.jsx
+++ 02 - 6 Pack programmer/01 - MASTER REACT JS IN ONE VIDEO WITH 5 PROJECTS/project-01/src/App.jsx
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom'
 import Header from './components/Header';
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
 
 
 import "./style/App.scss";
@@ -11,26 +13,28 @@ import "./style/header.scss";
 import "./style/home.scss";
 import "./style/footer.scss";
 import "./style/contact.scss";
+import "./style/services.scss";
+
 
 
 const App = () => {
   return (
     <div>
-
       <Router>
-         
-        <Header/>
+        <Header />
 
         <Routes>
-          <Route path='/' element= {<Home/>}>
-          </Route>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/contact" element={<Contact />} />
+
+          <Route path="/services" element={<Services/>} />
         </Routes>
 
-        <Footer/>
-      
+        <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
