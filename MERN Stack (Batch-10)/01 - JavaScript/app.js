6e34e6633bc150e7cd58aa8efd554be7b93056df
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-05-04 17:06:52 +0500

    update

---
+++ MERN Stack (Batch-10)/01 - JavaScript/app.js
@@ -0,0 +1,36 @@
+// for (i = 0; i < 10; i++) {
+//   document.write("Hello  <br> ");
+// }
+
+let person = {
+  name: "Ali",
+  age: 23,
+  class: "bs",
+};
+
+// let p= person;
+// console.log(person);
+
+// let {name,...r} = {...person}
+
+// console.log(name ,r);
+
+let names = [1, 2, 3, 4, 5];
+
+
+    // 💓 For loop 
+
+// for (i = names; i <= names; names++) {
+//   console.log(names);
+// }
+
+
+// 💓💓 Map
+
+
+names.map((val)=>{
+    console.log(val) ;
+
+
+})
+    console.log(names);
