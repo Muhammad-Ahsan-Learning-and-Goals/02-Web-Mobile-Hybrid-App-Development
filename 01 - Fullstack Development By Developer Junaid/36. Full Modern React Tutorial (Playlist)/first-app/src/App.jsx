8887819066fcc974d6cda07961021c787c6e43de
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-07-01 19:01:33 +0500

    add new app

--- 01 - Fullstack Development By Developer Junaid/36. Full Modern React Tutorial (Playlist)/first-app/src/App.jsx
+++ 01 - Fullstack Development By Developer Junaid/36. Full Modern React Tutorial (Playlist)/first-app/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 import First from "./components/First";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
-      <div>
-        <First />
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
+        {/* <First /> */}
       </div>
     </>
   );
---
+++ 01 - Fullstack Development By Developer Junaid/36. Full Modern React Tutorial (Playlist)/first-app/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <nav className="navbar">
+          <h1> This is Blog Page</h1>
+          <div className="links">
+            <a href="/">Home</a>
+            <a href="/create" style={{color:'white' ,
+             backgroundColor:'#f1356d', borderRadius:'8px'}}>New blog</a>
+
+
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
