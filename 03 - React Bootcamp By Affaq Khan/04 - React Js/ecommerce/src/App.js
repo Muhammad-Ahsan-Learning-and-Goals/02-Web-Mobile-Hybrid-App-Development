42f0932cc7a731538c0b44dcc8e9d5185349237f
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-08-24 10:51:31 +0500

    add new project

--- 03 - React Bootcamp By Affaq Khan/04 - React Js/ecommerce/src/App.js
+++ 03 - React Bootcamp By Affaq Khan/04 - React Js/ecommerce/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-import {Component1,Component2} from "./components/Component1";
-import Navbar from './components/Navbar';
+import { Component1, Component2 } from "./components/Component1";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <h3> This Is Main App Component </h3>
 
-      <Navbar/>
-
+      <Navbar />
+      <About />
+      <Contact />
+      <Products />
+      <Footer />
     </>
   );
 };
-
-
-
 
 export default App;
