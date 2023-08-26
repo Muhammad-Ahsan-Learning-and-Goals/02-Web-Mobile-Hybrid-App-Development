54c1bd97ec2e9143e3d706f0a18c6f1123a4eae5
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-08-26 09:19:29 +0500

    home component update

--- 03 - React Bootcamp By Affaq Khan/04 - React Js/nft project/src/pages/home/Home.jsx
+++ 03 - React Bootcamp By Affaq Khan/04 - React Js/nft project/src/pages/home/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
     <>
       <div className="container-fluid">
         <div className="row home-topbar">
-          <div className="col-lg-6 col-md-6 col-sm-12 left-col">
+          <div className="col-lg-6 col-md-6 col-sm-12 left-col mb-5 mt-5">
             <h1 className="white_color mt-3">
-              {" "}
+            
               Not Just Another Digital Agency
             </h1>
             <p className="white_color mt-2">
@@ -24,14 +24,18 @@ const Home = () => {
           </div>
 
           <div className="col-lg-6 col-md-6 col-sm-12 right-col">
-            <h1 className="white_color"> Top Categories</h1>
-            <hr />
-            {topCategories.map((Category) => (
-              <>
-                <img src={Category.img} alt="" />
-                <p>{Category.name}</p>
-              </>
-            ))}
+            <h1 className="white_color text-center"> Top Categories</h1>
+            <hr className="white_color" />
+            <div className="row">
+              {topCategories.map((Category) => (
+                <>
+                  <div className="col-6 col-lg-4 col-md--4 col-sm- text-center">
+                    <img src={Category.img} className="category-img" alt="" />
+                    <p className="white_color category_name"> {Category.name}</p>
+                  </div>
+                </>
+              ))}
+            </div>
           </div>
         </div>
       </div>
