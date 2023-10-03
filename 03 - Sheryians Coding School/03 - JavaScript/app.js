64d152e7bcdf503315a7cfc53565cf64bef2e100
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-10-04 01:05:30 +0500

    update new class

---
+++ 03 - Sheryians Coding School/03 - JavaScript/app.js
@@ -0,0 +1,31 @@
+// Hoisting
+
+console.log(a);
+var a = 10;
+
+// Types of Variable
+
+// refrance =[array],{ object},(),
+
+// permitive = number , string, null,  undefined , boolean,
+
+// let ab = 11;
+
+// if (ab >= 12) {
+//   console.log("You can Caste Vote");
+// } else {
+//   console.log("you Are not Eligible For Vote");
+// }
+
+// Loops
+
+// for (a = 0; a <= 10; a++) {
+//   console.log(a);
+// }
+
+
+ a= 0;
+while (a <= 10) {
+  console.log("True");
+  a++;
+}
